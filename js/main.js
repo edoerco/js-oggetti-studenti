262cb8c262d49c6b6a99c5aa0f572e25e216ebfa
Edoardo <ercolino.edoardo@gmail.com>
2021-07-30 16:08:24 +0200

    primi 3 punti fatti

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+var utente = {
+    nome : 'edoardo',
+    cognome : 'ercolino',
+    eta : 26
+};
+
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for(var key in utente) {
+    console.log(utente[key]);
+};
 // Creare un array di oggetti di studenti.
+var studenti = 
+[
+    {
+        nome : 'alberto',
+        cognome : 'rossi'
+    },
+    {
+        nome : 'dario',
+        cognome : 'gialli'
+    },
+    {
+        nome : 'federico',
+        cognome : 'verdi'
+    },
+    {
+        nome : 'elena',
+        cognome : 'bianchi'
+    },
+];
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for(var key in studenti) {
+    console.log(studenti[key])
+}
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-// Buon lavoro, vi ricordo che oggi avrete ancora la possibilità di aprire dei ticket per problematiche relative a questo esercizio 
+
